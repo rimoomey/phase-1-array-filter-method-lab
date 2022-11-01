9910e464233e35e7a2d2bd3577d7e95404d04530
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-01 16:40:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(collection, target) {
+    return collection.filter((element) => element.toUpperCase() == target.toUpperCase());
+}
+
+function fuzzyMatch(collection, letters) {
+    return collection.filter((el) => letters === el.slice(0, letters.length));
+}
+
+function matchName(collection, name) {
+    return collection.filter((el) => el.name === name);
+}
